ec38ec79ad985e29232ee416264ebde60077d1ef
Francisco Ascue Orosco <57104899+FranciscoAscue@users.noreply.github.com>
2023-09-04 18:42:14 -0500

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,11 +1,13 @@
-mkdir -p ngsexample/{data,results/{map,quality,trimmed},scripts}
-cd ngsexample/data
+dir.create(paste0(getwd(), "/data"))
+           dir.create(paste0(getwd(), "/data/reference"))
+                      url_download <- "https://raw.githubusercontent.com/FranciscoAscue/Rgenomics/master/data/reference/ref.json"
+                      download.file(url_download,paste0(getwd(),"/data/reference/ref.json"))
+           dir.create(paste0(getwd(), "/data/rawdata"))
+           dir.create(paste0(getwd(), "/data/processed_data"))
+dir.create(paste0(getwd(), "/scripts"))
+dir.create(paste0(getwd(), "/results"))
 
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1NNb6A00VqsP7ns6OUx9Awtoihynyubhm' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1NNb6A00VqsP7ns6OUx9Awtoihynyubhm" -O SRR21688981_mt_1.fq.gz && rm -rf /tmp/cookies.txt
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1HFoPzSdCzP-w_hXM7ALVMyJDhzn_Kh5M' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1HFoPzSdCzP-w_hXM7ALVMyJDhzn_Kh5M" -O SRR21688982_mt_1.fq.gz && rm -rf /tmp/cookies.txt
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1-_ARlYVMCByOdDVocUV9T92Zu93EtdJa' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1-_ARlYVMCByOdDVocUV9T92Zu93EtdJa" -O SRR21688985_mt_1.fq.gz && rm -rf /tmp/cookies.txt
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1zAaUUt_aAYrsRrCnZ8LRK8EWvOo70zPS' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1zAaUUt_aAYrsRrCnZ8LRK8EWvOo70zPS" -O SRR21688987_mt_1.fq.gz && rm -rf /tmp/cookies.txt
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1NNb6A00VqsP7ns6OUx9Awtoihynyubhm' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1NNb6A00VqsP7ns6OUx9Awtoihynyubhm" -O SRR21688981_mt_2.fq.gz && rm -rf /tmp/cookies.txt
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=18ysgV3xLQoWRXognUaC3RxcF5RP7pQ6Z' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=18ysgV3xLQoWRXognUaC3RxcF5RP7pQ6Z" -O SRR21688982_mt_2.fq.gz && rm -rf /tmp/cookies.txt
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1SDh7pzKZ7L4zx54fjmGye9JTry-R53mo' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1SDh7pzKZ7L4zx54fjmGye9JTry-R53mo" -O SRR21688985_mt_2.fq.gz && rm -rf /tmp/cookies.txt
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1Uv_VnVjOGuJfIUpDmPvJccu816Ua3Ob_' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1Uv_VnVjOGuJfIUpDmPvJccu816Ua3Ob_" -O SRR21688987_mt_2.fq.gz && rm -rf /tmp/cookies.txt
+url_download <- "https://raw.githubusercontent.com/FranciscoAscue/Rgenomics/master/install.R"
+download.file(url_download,paste0(getwd(),"/install.R"))
+url_download <- "https://raw.githubusercontent.com/FranciscoAscue/Rgenomics/master/main.R"
+download.file(url_download,paste0(getwd(),"/main.R"))
